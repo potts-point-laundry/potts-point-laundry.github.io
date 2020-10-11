93c32ba27015232fdc0a60d613d77234e7b38d15
Manjue Ruan <Manjue.Ruan-1@student.uts.edu.au>
2020-10-11 13:07:08 +1100

    added sitemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,48 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: [
-        './frontend/index.jsx'
-    ],
-    plugins: [
-        new MiniCssExtractPlugin({ filename: "[name].css" })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react"
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ],
-            },
-            {
-                test: /\.(png|jpg|jpeg)$/,
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images/',
-                    name: '[name].[ext]',
-                    publicPath: '/images'
-                }
-            }
-        ]
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin({
-                test: /\.js(\?.*)?$/i,
-            }),
-        ],
-    },
-    output: {
-        path: __dirname + '/docs',
-        filename: 'bundle.js'
-    }
+	entry: ["./frontend/index.jsx"],
+	plugins: [new MiniCssExtractPlugin({ filename: "[name].css" })],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: ["/node_modules/", "/lib/"],
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+			{
+				test: /\.(png|jpg|jpeg)$/,
+				loader: "file-loader",
+				options: {
+					outputPath: "images/",
+					name: "[name].[ext]",
+					publicPath: "/images",
+				},
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new TerserPlugin({
+				test: /\.js(\?.*)?$/i,
+			}),
+		],
+	},
+	output: {
+		path: __dirname + "/docs",
+		filename: "bundle.js",
+	},
 };
