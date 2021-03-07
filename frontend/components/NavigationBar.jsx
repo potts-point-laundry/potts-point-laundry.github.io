5bff02f27a2f1cc1580aaaf39e601e8da624e14f
Jimmy Ruan <jimmyruan@hotmail.com>
2021-03-07 15:03:22 +1100

    status bar

--- frontend/components/NavigationBar.jsx
+++ frontend/components/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, Alert } from "react-bootstrap";
 
 export default function NavigationBar({ bg = "dark", variant = "dark" }) {
 	// window.location.pathname
@@ -11,28 +11,32 @@ export default function NavigationBar({ bg = "dark", variant = "dark" }) {
 	];
 
 	return (
-		<Navbar collapseOnSelect expand="lg" bg={bg} variant={variant} className="web-theme-bg-color">
-			<Container>
-				<Navbar.Brand href="/" className="nav-brand">
-					Potts Point Laundry & Dry Cleaning
-				</Navbar.Brand>
-				<Navbar.Toggle />
-				<Navbar.Collapse className="justify-content-end">
-					<Nav>
-						{paths.map((each, i) => {
-							return (
-								<Nav.Link
-									key={i}
-									href={each.path}
-									active={window.location.pathname === each.path ? true : false}
-								>
-									{each.name.toUpperCase()}
-								</Nav.Link>
-							);
-						})}
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+		<>
+			<Navbar collapseOnSelect expand="lg" bg={bg} variant={variant} className="web-theme-bg-color">
+				<Container>
+					<Navbar.Brand href="/" className="nav-brand">
+						Potts Point Laundry & Dry Cleaning
+					</Navbar.Brand>
+					<Navbar.Toggle />
+					<Navbar.Collapse className="justify-content-end">
+						<Nav>
+							{paths.map((each, i) => {
+								return (
+									<Nav.Link
+										key={i}
+										href={each.path}
+										active={window.location.pathname === each.path ? true : false}>
+										{each.name.toUpperCase()}
+									</Nav.Link>
+								);
+							})}
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+			<Alert variant="info" className="text-center rounded-0 m-0 font-weight-bold">
+				Sensitive skin laundry powder available upon request.
+			</Alert>
+		</>
 	);
 }
